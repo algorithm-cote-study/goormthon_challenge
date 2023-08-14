03cae8b3b441f0a548296a0b5e0bebf69afb6015
seunggulee1007 <32692807+seunggulee1007@users.noreply.github.com>
2023-08-14 21:38:30 +0900

    feat: 운동 중독 플레이어 문제 풀이 추가

--- src/main/java/com/goorm/week1/day1/seunggu/Solution.java
+++ src/main/java/com/goorm/week1/day1/seunggu/Solution.java
@@ -1,2 +1,23 @@
-package com.goorm.week1.day1.seunggu;public class Solution {
+package com.goorm.week1.day1.seunggu;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.println(solution(br));
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static int solution(BufferedReader br) throws IOException {
+        String line = br.readLine();
+        StringTokenizer stringTokenizer = new StringTokenizer(line, " ");
+        double weight = Double.parseDouble(stringTokenizer.nextToken());
+        double count = Double.parseDouble(stringTokenizer.nextToken());
+        double rm = weight * (1 + count / 30);
+        return (int)rm;
+    }
 }
