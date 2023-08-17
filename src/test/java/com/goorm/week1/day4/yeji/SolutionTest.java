cdaa453ba6a48529b4de9cd496d3a26e0610b399
cyeji <choyeji1591@gmail.com>
2023-08-17 10:33:25 +0900

    test : 완벽한 햄버거 만들기 테스트 추가

---
+++ src/test/java/com/goorm/week1/day4/yeji/SolutionTest.java
@@ -0,0 +1,36 @@
+package com.goorm.week1.day4.yeji;
+
+import static com.goorm.week1.day4.yeji.Solution.solution;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.goorm.common.TestFileUtil;
+import java.io.BufferedReader;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("완벽한 햄버거 만들기 - 예지")
+class SolutionTest {
+
+
+    @Test
+    @DisplayName("완벽한 햄버거 만들기 - 케이스1")
+    void test_case_1() throws Exception {
+        // given
+        BufferedReader reader = TestFileUtil.getReader(this.getClass(), "testcase/week1/day5/test_case1.txt");
+        // when
+        int solution = solution(reader);
+        // then
+        assertEquals(10, solution);
+    }
+
+    @Test
+    @DisplayName("완벽한 햄버거 만들기 - 케이스2")
+    void test_case_2() throws Exception {
+        // given
+        BufferedReader reader = TestFileUtil.getReader(this.getClass(), "testcase/week1/day5/test_case2.txt");
+        // when
+        int solution = solution(reader);
+        // then
+        assertEquals(0, solution);
+    }
+}
