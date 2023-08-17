c578349359d0bc6f9d067c646f89b1eb244ce32a
songjuAhn <ggb06104@naver.com>
2023-08-17 19:03:37 +0900

    test: 완벽한 햄버거 풀이 추가

---
+++ src/test/java/com/goorm/week1/day4/songju/SolutionTest.java
@@ -0,0 +1,28 @@
+package com.goorm.week1.day4.songju;
+
+import com.goorm.common.TestFileUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("완벽한 햄버거 만들기")
+class SolutionTest {
+    @Test
+    @DisplayName("완벽한 햄버거 만들기 테스트 케이스1")
+    void test_case_1() throws IOException {
+        BufferedReader reader = TestFileUtil.getReader(this.getClass(), "week1/day4/test_case1.txt");
+        int solution = Solution.solution(reader);
+        assertEquals(10, solution);
+    }
+    @Test
+    @DisplayName("완벽한 햄버거 만들기 테스트 케이스2")
+    void test_case_2() throws IOException {
+        BufferedReader reader = TestFileUtil.getReader(this.getClass(), "week1/day4/test_case2.txt");
+        int solution = Solution.solution(reader);
+        assertEquals(0, solution);
+    }
+}
