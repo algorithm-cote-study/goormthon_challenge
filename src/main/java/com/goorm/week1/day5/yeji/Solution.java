424a4e129d6c821c455182ca781030a8a46ef3f9
cyeji <choyeji1591@gmail.com>
2023-08-18 10:41:21 +0900

    feat : 이진수 정렬 풀이 추가

---
+++ src/main/java/com/goorm/week1/day5/yeji/Solution.java
@@ -0,0 +1,72 @@
+package com.goorm.week1.day5.yeji;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * 이진수 정렬 (195687)
+ * 14'27"
+ */
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.print(solution(reader));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static int solution(BufferedReader br) throws IOException {
+        int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] nums = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        Integer cur = sortedBinary(input, nums);
+        if (cur != null) return cur;
+        return 0;
+    }
+
+    private static Integer sortedBinary(int[] input, int[] nums) {
+        Queue<Integer> priorityQueue = new PriorityQueue<>((a, b) -> {
+            int binaryA = 0;
+            int binaryB = 0;
+
+            int compareA = a;
+            while (compareA != 0) {
+                if (compareA % 2 == 1) {
+                    binaryA++;
+                }
+                compareA /= 2;
+            }
+
+            int compareB = b;
+            while (compareB != 0) {
+                if (compareB % 2 == 1) {
+                    binaryB++;
+                }
+                compareB /= 2;
+            }
+
+            if (binaryA == binaryB) return b - a;
+            return binaryB - binaryA;
+        });
+
+        for (int num : nums) {
+            priorityQueue.offer(num);
+        }
+
+        int target = 1;
+        while (!priorityQueue.isEmpty()) {
+            int cur = priorityQueue.poll();
+            if (target == input[1]) {
+                return cur;
+            }
+            target++;
+        }
+        return null;
+    }
+}
