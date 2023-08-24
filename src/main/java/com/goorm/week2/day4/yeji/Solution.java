fcda9f29586aba36f4cc5a2dd672076c9a627910
cyeji <choyeji1591@gmail.com>
2023-08-24 12:49:40 +0900

    feat : 폭탄 구현하기 풀이 추가

---
+++ src/main/java/com/goorm/week2/day4/yeji/Solution.java
@@ -0,0 +1,74 @@
+package com.goorm.week2.day4.yeji;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * 폭탄 구현하기 (2)
+ * 8' 23"
+ */
+public class Solution {
+
+    private static final int[] dx = {1, 0, -1, 0};
+    private static final int[] dy = {0, 1, 0, -1};
+
+    public static void main(String[] args) throws Exception {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.print(solution(reader));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static int solution(BufferedReader br) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int bombCount = Integer.parseInt(st.nextToken());
+        String[][] map = new String[n][n];
+        int[][] bombMap = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            map[i] = br.readLine().split(" ");
+        }
+
+        for (int i = 0; i < bombCount; i++) {
+            int[] location = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            popingBomb(location[0] - 1, location[1] - 1, map, bombMap, n);
+        }
+
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                max = Math.max(max, bombMap[i][j]);
+            }
+        }
+
+        return max;
+    }
+
+    private static void popingBomb(int x, int y, String[][] map, int[][] bombMap, int n) {
+        if ("0".equals(map[x][y])) {
+            bombMap[x][y]++;
+        } else if ("@".equals(map[x][y])) {
+            bombMap[x][y] += 2;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            int cx = x + dx[i];
+            int cy = y + dy[i];
+
+            if (cx >= 0 && cx < n && cy >= 0 && cy < n && !"#".equals(map[cx][cy])) {
+                if ("0".equals(map[cx][cy])) {
+                    bombMap[cx][cy]++;
+                } else if ("@".equals(map[cx][cy])) {
+                    bombMap[cx][cy] += 2;
+                }
+            }
+
+        }
+
+    }
+}
